992dc99bf82d8412133201d883f01a7653569388
rapPayne <bob@creator.net>
2024-11-15 17:44:43 -0600

    Mismatched version of RSS feed module

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,21 @@
 import rss from '@astrojs/rss';
 
 export async function GET(context) {
-  const blog = await context.glob('./blog/*.md');
+  // Glob all blog posts
+  const blogPosts = import.meta.glob('./blog/*.md', { eager: true });
+
+  // Map and transform the blog post data
+  const items = Object.values(blogPosts).map((post) => ({
+    title: post.frontmatter.title,
+    pubDate: post.frontmatter.pubDate,
+    description: post.frontmatter.description,
+    link: `/blog/${post.frontmatter.slug || post.file.split('/').pop().replace('.md', '')}/`,
+  }));
+
   return rss({
-    title: 'My Blog',
-    description: 'A blog built with Astro',
+    title: 'Agile Gadgets by Rap Payne',
+    description: `A blog site for Rap's writing, consulting, mentoring and training`,
     site: context.site,
-    items: blog.map((post) => ({
-      title: post.frontmatter.title,
-      pubDate: new Date(post.frontmatter.date),
-      description: post.frontmatter.description,
-      link: post.url,
-    })),
+    items,
   });
 }
