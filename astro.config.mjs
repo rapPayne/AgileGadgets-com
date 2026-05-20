b945c93300fa1fd9e05e4aa9edda41761e947426
rapPayne <bob@creator.net>
2026-05-19 21:13:30 -0500

    Made Mermaid work

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,18 @@
 import { defineConfig } from 'astro/config'
 
 import netlify from '@astrojs/netlify';
+import rehypeMermaid from 'rehype-mermaid';
 
 export default defineConfig({
   site: 'https://agilegadgets.com',
   base: '/',
   output: 'server',
   adapter: netlify(),
+  markdown: {
+    syntaxHighlight: {
+      type: 'shiki',
+      excludeLangs: ['mermaid'],
+    },
+    rehypePlugins: [rehypeMermaid],
+  },
 })
