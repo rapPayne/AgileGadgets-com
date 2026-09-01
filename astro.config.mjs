8b88db73781ec3f8577759e375ae3ad1e267af95
rapPayne <bob@creator.net>
2026-09-01 14:52:55 -0500

    Converted to static pages from server-rendered

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
-import netlify from '@astrojs/netlify';
-
 export default defineConfig({
   site: 'https://agilegadgets.com',
   base: '/',
-  output: 'server',
-  adapter: netlify(),
+  output: 'static',
 })
